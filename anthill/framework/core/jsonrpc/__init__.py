84c401ad86d79164024fb8cde773e6e83aa1605e
Vladimir <x55aah@gmail.com>
2018-05-16 18:39:17 +0300

    add json-rpc protocol

---
+++ anthill/framework/core/jsonrpc/__init__.py
@@ -0,0 +1,9 @@
+from .manager import JSONRPCResponseManager
+from .dispatcher import Dispatcher
+
+__version = (1, 11, 0)
+
+__version__ = version = '.'.join(map(str, __version))
+__project__ = PROJECT = __name__
+
+dispatcher = Dispatcher()
