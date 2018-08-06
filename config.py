b4e42e5f19c06d324f1f26e2bed73188a03ea3ed
Vladimir Lysenko <x55aah@gmail.com>
2018-08-06 10:32:16 +0300

    add api backward compatibility

--- config.py
+++ config.py
@@ -1,18 +1,1 @@
 CONFIG_MODULE = 'anthill.platform.conf'
-
-PORTS_POOL = range(9500, 9600)
-
-
-def get_allowed_ports():
-    return PORTS_POOL
-
-
-def get_taken_ports():
-    return []
-
-
-def get_next_free_port():
-    port = max(get_taken_ports()) + 1
-    if port > max(get_allowed_ports()) + 1:
-        raise ValueError('All ports in pool is allready taken.')
-    return port
--- login/routes.py
+++ login/routes.py
@@ -2,9 +2,12 @@
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
 from . import handlers
+from discovery.api.compat.rest import routes as rest_routes
+from anthill.framework.utils.urls import include
 
 # Create your routes here.
 route_patterns = [
+    url(r'^/', include(rest_routes.route_patterns, namespace='api')),  # for compatibility only
     url(r'^/login/?$', handlers.LoginHandler, name='login'),
     url(r'^/logout/?$', handlers.LogoutHandler, name='logout'),
     url(r'^/register/?$', handlers.RegisterHandler, name='register'),
