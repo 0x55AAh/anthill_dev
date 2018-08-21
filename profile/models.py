3f36123248a0055cb79454c15b8a5fe1f0997061
Vladimir <x55aah@gmail.com>
2018-08-21 20:35:03 +0300

    update profile with JSONField

--- profile/models.py
+++ profile/models.py
@@ -1,6 +1,7 @@
 # For more details, see
 # http://docs.sqlalchemy.org/en/latest/orm/tutorial.html#declare-a-mapping
 from anthill.framework.db import db
+from sqlalchemy_jsonfield import JSONField
 
 
 class Profile(db.Model):
