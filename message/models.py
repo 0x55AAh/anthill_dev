d49739e2a12e5a8936ab8679e5026716ea4ecdc7
Vladimir Lysenko <x55aah@gmail.com>
2018-10-11 11:18:19 +0300

    init messenger models

--- message/models.py
+++ message/models.py
@@ -2,4 +2,14 @@
 # http://docs.sqlalchemy.org/en/latest/orm/tutorial.html#declare-a-mapping
 from anthill.framework.db import db
 
-# Create your models here.
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
