429a23730be2ea878f3b1a686e59b98dd0a62c8e
Vladimir Lysenko <x55aah@gmail.com>
2018-10-12 11:07:41 +0300

    update messenger db client

--- anthill/platform/core/messenger/client/backends/db.py
+++ anthill/platform/core/messenger/client/backends/db.py
@@ -1,4 +1,5 @@
 from anthill.platform.core.messenger.client.backends.base import BaseClient
+from typing import Optional
 
 
 class Client(BaseClient):
@@ -6,44 +7,44 @@ class Client(BaseClient):
         """Returns user as dict object."""
         return self.user.to_dict()
 
-    async def get_friends(self, id_only=False):
+    async def get_friends(self, id_only: bool=False) -> list:
         pass
 
-    async def get_groups(self):
+    async def get_groups(self) -> list:
         pass
 
-    async def create_group(self, group_name, group_data):
+    async def create_group(self, group_name: str, group_data: dict) -> str:
         pass
 
-    async def delete_group(self, group_name):
+    async def delete_group(self, group_name: str) -> None:
         pass
 
-    async def update_group(self, group_name, group_data):
+    async def update_group(self, group_name: str, group_data: dict) -> None:
         pass
 
-    async def join_group(self, group_name):
+    async def join_group(self, group_name: str) -> None:
         pass
 
-    async def leave_group(self, group_name):
+    async def leave_group(self, group_name: str) -> None:
         pass
 
-    async def enumerate_group(self, group, new=None):
+    async def enumerate_group(self, group: str, new=None) -> list:
         pass
 
-    async def create_message(self, group, message):
+    async def create_message(self, group: str, message: dict) -> str:
         pass
 
-    async def get_messages(self, group, message_ids):
+    async def get_messages(self, group: str, message_ids: list) -> list:
         pass
 
-    async def delete_messages(self, group, message_ids):
+    async def delete_messages(self, group: str, message_ids: list):
         pass
 
-    async def update_messages(self, group, messages_data):
+    async def update_messages(self, group: str, messages_data: dict):
         pass
 
-    async def read_messages(self, group, message_ids):
+    async def read_messages(self, group: str, message_ids: list):
         pass
 
-    async def forward_messages(self, group, message_ids, group_to):
+    async def forward_messages(self, group: str, message_ids: list, group_to: str):
         pass
