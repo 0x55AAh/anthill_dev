86e1d4a2c42124c448db301c694e188022d36c28
Vladimir Lysenko <x55aah@gmail.com>
2018-10-19 10:04:38 +0300

    fix auth

--- anthill/framework/auth/backends.py
+++ anthill/framework/auth/backends.py
@@ -15,7 +15,7 @@ class ModelBackend:
         if user is None:
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user.
-            UserModel().set_password(password)
+            UserModel(username=None).set_password(password)
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
