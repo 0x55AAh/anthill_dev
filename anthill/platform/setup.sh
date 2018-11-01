497bc0bbc2884fd5c1015861fde310eb7db85e32
Vladimir <x55aah@gmail.com>
2018-11-01 20:04:45 +0200

    update scripts

--- anthill/platform/setup.sh
+++ anthill/platform/setup.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
-apt install redis-server -y
-apt install rabbitmq-server -y
-apt install postgresql -y
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    apt install redis-server -y
+    apt install rabbitmq-server -y
+    apt install postgresql -y
+
+    service start redis-server
+    service start rabbitmq-server
+    service start postgresql
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install redis
+    brew install rabbitmq
+    brew install postgres
+
+    brew services start redis
+    brew services start rabbitmq
+    brew services start postgres
+fi
+
--- setup.sh
+++ setup.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
+OS=${OSTYPE//[0-9.-]*/}
+
+case "$OS" in
+    darwin|linux)
+        echo "Starting installation script...";;
+    *)
+        echo "Unknown operating system $OSTYPE"
+        exit 1
+esac
+
+if [[ "$OS" == "darwin" ]]; then
+    if [[ $EUID -eq 0 ]]; then
+        echo "Do not run this script as root." 2>&1
+        exit 1
+    fi
+fi
+
+# General setup
 source anthill/framework/setup.sh
 source anthill/platform/setup.sh
+
+# Services setup
 source admin/setup.sh
 source config/setup.sh
 source discovery/setup.sh
