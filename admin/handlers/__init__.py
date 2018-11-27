5d441ac54b300b6eaf19f8bd9af57c9e17275505
Vladimir Lysenko <x55aah@gmail.com>
2018-11-27 23:45:47 +0200

    update admin ui

--- admin/handlers/__init__.py
+++ admin/handlers/__init__.py
@@ -138,11 +138,14 @@ class SidebarMainToggle(UserHandlerMixin, RequestHandler):
 
 
 class ServiceContextMixin:
+    async def get_service_metadata(self):
+        service_name = self.path_kwargs['name']
+        return await self.internal_request(service_name, method='get_service_metadata')
+
     async def get_context_data(self, **kwargs):
         context = await super().get_context_data(**kwargs)
         try:
-            metadata = await self.internal_request(
-                self.path_kwargs['name'], method='get_service_metadata')
+            metadata = await self.get_service_metadata()
         except RequestTimeoutError:
             metadata = {}
         context.update(metadata=metadata)
@@ -167,10 +170,10 @@ class ServiceRequestHandler(ServiceContextMixin, UserTemplateHandler):
 
 
 # @authenticated(methods=['GET'])
-class SettingsRequestHandler(UserTemplateHandler):
-    template_name = 'settings.html'
+class LogRequestHandler(ServiceContextMixin, UserTemplateHandler):
+    template_name = 'log.html'
 
 
 # @authenticated(methods=['GET'])
-class LogRequestHandler(ServiceContextMixin, UserTemplateHandler):
-    template_name = 'log.html'
+class SettingsRequestHandler(UserTemplateHandler):
+    template_name = 'settings.html'
