7a0199dbcae01a4fcd3ad138eb3bc7ccf0f38fe3
Vladimir <x55aah@gmail.com>
2018-11-29 16:16:16 +0200

    update admin ui

--- admin/routes/event.py
+++ admin/routes/event.py
@@ -1,7 +1,7 @@
 # For more details about routing, see
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
-from admin.handlers import exec as handlers
+from admin.handlers import event as handlers
 
 
 route_patterns = [
