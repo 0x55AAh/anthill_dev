6eb6b7067e333026e2383b3a62e6bdb6b3964d76
Vladimir <x55aah@gmail.com>
2018-12-05 20:02:48 +0200

    update platform setup.sh

--- anthill/platform/setup.sh
+++ anthill/platform/setup.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+REDIS_DATABASES=32
+
+function update_redis_databases {
+    sed -iE "s/^databases [0-9]*$/databases ${REDIS_DATABASES}/" $1
+}
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     apt install redis-server -y
     apt install rabbitmq-server -y
     apt install postgresql -y
+
+    update_redis_databases /etc/redis/redis.conf
 
     service start redis-server
     service start rabbitmq-server
@@ -13,6 +21,8 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install redis
     brew install rabbitmq
     brew install postgres
+
+    update_redis_databases /usr/local/etc/redis.conf
 
     brew services start redis
     brew services start rabbitmq
