8766332d8082915c91f8a9ea5448a1263e0222f7
Vladimir <x55aah@gmail.com>
2018-12-21 20:03:05 +0200

    update admin ui

--- admin/ui/themes/limitless/static/js/pages/log.js
+++ admin/ui/themes/limitless/static/js/pages/log.js
@@ -7,6 +7,9 @@
 * ---------------------------------------------------------------------------- */
 
 $(function() {
+
+    var LOG_RE = /^(\[.*?\])\s+(.+)$/;
+
     var term = $('.console').terminal(function() {}, {
         enable: false,
         greetings: null,
@@ -20,12 +23,24 @@ $(function() {
         maxRetries: 10,
         debug: window.debug
     };
-    var url = 'ws://localhost:9500/log/';
+    var url = 'ws://localhost:9600/log/';
     var client = new ReconnectingWebSocket(url, [], options);
     client.addEventListener('message', function(event) {
-        term.echo(event.data);
+        var parsed = event.data.match(LOG_RE);
+        var prefix = parsed[1], message = parsed[2];
+        var prefix_parts = prefix.match(/^\[(.+)\]$/)[1].split(/\s+/);
+        var mode = prefix_parts[0],
+            date = prefix_parts[1],
+            time = prefix_parts[2],
+            datetime = Date.parse([date, time].join(" ")),
+            user = null;
+        if (prefix_parts.length === 5) {
+            user = prefix_parts[3];
+        }
+        term.echo([prefix, message].join(' '));
     });
     client.addEventListener('error', function(event) {
         // ¯\_(ツ)_/¯
     });
+
 });
