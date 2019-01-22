31e2c17f97662ac7eaccf63b726cd1317f261b4c
Vladimir Lysenko <x55aah@gmail.com>
2019-01-22 10:10:07 +0200

    update moderation models

--- moderation/models.py
+++ moderation/models.py
@@ -2,8 +2,11 @@
 # http://docs.sqlalchemy.org/en/latest/orm/tutorial.html#declare-a-mapping
 from anthill.framework.db import db
 from anthill.platform.api.internal import InternalAPIMixin
+from anthill.platform.auth import RemoteUser
 from sqlalchemy_utils.types.json import JSONType
 from anthill.framework.utils import timezone
+from typing import Optional
+from datetime import timedelta
 import enum
 
 
@@ -27,35 +30,34 @@ class ModerationAction(InternalAPIMixin, db.Model):
     reason = db.Column(db.String(512), nullable=False)
     extra_data = db.Column(JSONType, nullable=False, default={})
 
-    async def get_user(self):
+    async def get_user(self) -> RemoteUser:
         return await self.internal_request(
             'login', 'get_user', user_id=self.user_id, include_profile=False)
 
-    async def get_moderator(self):
+    async def get_moderator(self) -> RemoteUser:
         return await self.internal_request(
             'login', 'get_user', user_id=self.moderator_id, include_profile=False)
 
-    def turn_on(self, commit=False):
+    def turn_on(self, commit: bool = False) -> None:
         self.is_active = True
         self.save(commit)
 
-    def turn_off(self, commit=False):
+    def turn_off(self, commit: bool = False) -> None:
         self.is_active = False
         self.save(commit)
 
-    def time_limited(self):
+    def time_limited(self) -> bool:
         return self.finish_at is not None
 
-    def finish_in(self):
+    def finish_in(self) -> Optional[timedelta]:
         if self.time_limited():
-            return self.finish_at - timezone.now
+            return self.finish_at - timezone.now()
 
-    def finished(self):
-        finish_in = self.finish_in()
-        if finish_in:
-            return finish_in.days < 0
+    def finished(self) -> Optional[bool]:
+        if self.time_limited():
+            return self.finish_at > timezone.now()
 
-    def active(self):
+    def active(self) -> bool:
         finished = self.finished()
         if finished is None:
             return self.is_active
