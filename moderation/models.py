760aad07fa7cba524720aa88232cbb6c0b67dbd2
Vladimir Lysenko <x55aah@gmail.com>
2019-01-23 02:31:38 +0200

    update moderation models

--- moderation/models.py
+++ moderation/models.py
@@ -2,6 +2,7 @@
 # http://docs.sqlalchemy.org/en/latest/orm/tutorial.html#declare-a-mapping
 from anthill.framework.db import db
 from anthill.platform.api.internal import InternalAPIMixin
+from anthill.framework.utils.asynchronous import as_future
 from anthill.platform.auth import RemoteUser
 from sqlalchemy_utils.types.json import JSONType
 from anthill.framework.utils import timezone
@@ -38,10 +39,12 @@ class ModerationAction(InternalAPIMixin, db.Model):
         return await self.internal_request(
             'login', 'get_user', user_id=self.moderator_id, include_profile=False)
 
+    @as_future
     def turn_on(self, commit: bool = False) -> None:
         self.is_active = True
         self.save(commit)
 
+    @as_future
     def turn_off(self, commit: bool = False) -> None:
         self.is_active = False
         self.save(commit)
