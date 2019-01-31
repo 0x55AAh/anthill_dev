ab8b267fe1a4249142c38204549f48215769bf70
Vladimir <x55aah@gmail.com>
2019-01-31 20:05:46 +0200

    general update (minor update)

--- anthill/platform/security/moderation.py
+++ anthill/platform/security/moderation.py
@@ -13,13 +13,6 @@ class ModerationError(Exception):
         self.action_types = action_types
 
 
-async def _run_func(func, *args, **kwargs):
-    if inspect.iscoroutinefunction(func):
-        return await func(*args, **kwargs)
-    else:
-        return func(*args, **kwargs)
-
-
 async def _check_user_action_types(handler, required_action_types):
     required_action_types = set(required_action_types)
     get_moderations = functools.partial(
@@ -36,6 +29,9 @@ def moderated(action_types: Union[list, tuple]):
     def decorator(func):
         async def wrapper(*args, **kwargs):
             await _check_user_action_types(args[0], action_types)
-            return await _run_func(func, *args, **kwargs)
+            if inspect.iscoroutinefunction(func):
+                return await func(*args, **kwargs)
+            else:
+                return func(*args, **kwargs)
         return wrapper
     return decorator
