6bd812f1b398cc0c302d9c61289033a5ef0a1490
Vladimir Lysenko <x55aah@gmail.com>
2019-02-11 00:35:48 +0200

    update event models

--- event/models.py
+++ event/models.py
@@ -4,12 +4,13 @@ from anthill.framework.db import db
 from anthill.framework.utils import timezone
 from anthill.platform.api.internal import InternalAPIMixin
 from sqlalchemy_utils.types.json import JSONType
+from sqlalchemy_utils.types.uuid import UUIDType
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.event import listens_for
+from celery.worker.control import revoke
+from typing import Optional
+from datetime import timedelta
 import enum
-
-
-@enum.unique
-class EventOnFinishActions(enum.Enum):
-    pass
 
 
 @enum.unique
@@ -24,10 +25,87 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     category_id = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, nullable=False, default=timezone.now)
+    start_at = db.Column(db.DateTime, nullable=False)
     finish_at = db.Column(db.DateTime, nullable=False)
     payload = db.Column(JSONType, nullable=False, default={})
     is_active = db.Column(db.Boolean, nullable=False, default=True)
-    on_finish = db.Column(db.Enum(EventOnFinishActions))
+
+    on_start_task_id = db.Column(UUIDType(binary=False))
+    on_finish_task_id = db.Column(UUIDType(binary=False))
+
+    @hybrid_property
+    def active(self) -> bool:
+        return self.finish_at > timezone.now() >= self.on_start() and self.is_active
+
+    @hybrid_property
+    def started(self) -> bool:
+        return self.start_at >= timezone.now()
+
+    @hybrid_property
+    def finished(self) -> bool:
+        return self.finish_at < timezone.now()
+
+    @hybrid_property
+    def start_in(self) -> Optional[timedelta]:
+        if self.start_at >= timezone.now():
+            return self.start_at - timezone.now()
+
+    @hybrid_property
+    def finish_in(self) -> Optional[timedelta]:
+        if self.finish_at >= timezone.now():
+            return self.finish_at - timezone.now()
+
+    async def on_start(self) -> None:
+        pass
+
+    async def on_finish(self) -> None:
+        pass
+
+
+@listens_for(Event, 'after_insert')
+def on_event_create(mapper, connection, target):
+    from event import tasks
+
+    if target.start_in:
+        task = tasks.on_event_start.apply_async(
+            [target.id], countdown=target.start_in.seconds)
+        target.on_start_task_id = task.id
+
+    if target.finish_in:
+        task = tasks.on_event_finish.apply_async(
+            [target.id], countdown=target.finish_in.seconds)
+        target.on_finish_task_id = task.id
+
+
+@listens_for(Event, 'after_update')
+def on_event_update(mapper, connection, target):
+    from event import tasks
+    from anthill.platform.core.celery import app
+
+    if target.on_start_task_id:
+        revoke(app, target.on_start_task_id)
+    if target.on_finish_task_id:
+        revoke(app, target.on_finish_task_id)
+
+    if target.start_in:
+        task = tasks.on_event_start.apply_async(
+            [target.id], countdown=target.start_in.seconds)
+        target.on_start_task_id = task.id
+
+    if target.finish_in:
+        task = tasks.on_event_finish.apply_async(
+            [target.id], countdown=target.finish_in.seconds)
+        target.on_finish_task_id = task.id
+
+
+@listens_for(Event, 'after_delete')
+def on_event_delete(mapper, connection, target):
+    from anthill.platform.core.celery import app
+
+    if target.on_start_task_id:
+        revoke(app, target.on_start_task_id)
+    if target.on_finish_task_id:
+        revoke(app, target.on_finish_task_id)
 
 
 class EventCategory(db.Model):
--- event/tasks.py
+++ event/tasks.py
@@ -7,5 +7,20 @@ Example:
 """
 
 from anthill.platform.core.celery import app
+from tornado.ioloop import IOLoop
 
-# Create your celery tasks here
+
+@app.task(ignore_result=True)
+def on_event_start(event_id):
+    from event.models import Event
+    event = Event.query.get(event_id)
+    if event is not None:
+        IOLoop.current().add_callback(event.on_start)
+
+
+@app.task(ignore_result=True)
+def on_event_finish(event_id):
+    from event.models import Event
+    event = Event.query.get(event_id)
+    if event is not None:
+        IOLoop.current().add_callback(event.on_finish)
