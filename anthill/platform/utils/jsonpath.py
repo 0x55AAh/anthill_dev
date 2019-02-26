df2d6dc723a32a3bb9df3832bdeb9772d8cac6b5
Vladimir <x55aah@gmail.com>
2019-02-26 20:26:41 +0200

    update jsonpath from jsonpath2 to jsonpath-ng

--- anthill/platform/utils/jsonpath.py
+++
@@ -1,4 +0,0 @@
-from jsonpath2.path import Path as _Path
-
-
-Path = _Path
--- profile/models.py
+++ profile/models.py
@@ -1,8 +1,11 @@
 # For more details, see
 # http://docs.sqlalchemy.org/en/latest/orm/tutorial.html#declare-a-mapping
 from anthill.framework.db import db
+from anthill.framework.conf import settings
+from anthill.framework.utils.asynchronous import as_future
 from anthill.platform.api.internal import InternalAPIMixin
 from sqlalchemy_utils.types.json import JSONType
+from jsonpath_ng.ext import parser
 
 
 class Profile(InternalAPIMixin, db.Model):
@@ -15,3 +18,16 @@ class Profile(InternalAPIMixin, db.Model):
 
     async def get_user(self):
         return await self.internal_request('login', 'get_user', user_id=self.user_id)
+
+    @as_future
+    def find_payload(self, path: str):
+        return parser.parse(path, debug=settings.DEBUG).find(self.payload)
+
+    @as_future
+    def filter_payload(self, path: str, fn):
+        return parser.parse(path, debug=settings.DEBUG).filter(fn, self.payload)
+
+    @as_future
+    def update_payload(self, path: str, data, commit=True):
+        self.payload = parser.parse(path, debug=settings.DEBUG).update(self.payload, data)
+        self.save(commit)
