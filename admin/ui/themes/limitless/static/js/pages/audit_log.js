000026360d237a92ab3be7b9700090aa0a221077
Vladimir Lysenko <x55aah@gmail.com>
2019-02-27 03:46:55 +0200

    minor updates profile models + update ui

--- admin/ui/themes/limitless/static/js/pages/audit_log.js
+++ admin/ui/themes/limitless/static/js/pages/audit_log.js
@@ -1,126 +1,63 @@
-/* ------------------------------------------------------------------------------
-*
-*  # Task list view
-*
-*  Specific JS code additions for task_manager_list.html page
-*
-*  Version: 1.0
-*  Latest update: Aug 1, 2015
-*
-* ---------------------------------------------------------------------------- */
-
-
-// Create an array with the values of all the input boxes in a column
-$.fn.dataTable.ext.order['dom-text'] = function (settings, col) {
-    return this.api().column(col, {order: 'index'}).nodes().map(function (td, i) {
-        return $('input', td).val();
-    });
-};
-
-
-// Create an array with the values of all the select options in a column
-$.fn.dataTable.ext.order['dom-select'] = function (settings, col) {
-    return this.api().column(col, {order: 'index'}).nodes().map(function (td, i) {
-        return $('select', td).val();
-    });
-};
-
-
-$(function () {
-
+$(function() {
 
     // Table setup
     // ------------------------------
 
-    // Initialize data table
-    $('.tasks-list').DataTable({
+    // Initialize
+    $('.table-customers').DataTable({
         autoWidth: false,
         columnDefs: [
             {
-                type: "natural",
-                width: '20px',
-                targets: 0
-            },
-            {
-                visible: false,
-                targets: 1
-            },
-            {
-                width: '40%',
-                targets: 2
-            },
-            {
-                width: '10%',
-                targets: 3
-            },
-            {
-                orderDataType: 'dom-text',
-                type: 'string',
-                targets: 4
-            },
-            {
-                orderDataType: 'dom-select',
-                type: 'string',
-                targets: 5
+                targets: 0,
+                width: 400
             },
             {
                 orderable: false,
-                width: '100px',
-                targets: 7
+                width: 16,
+                targets: 6
             },
             {
-                width: '15%',
-                targets: [4, 5, 6]
+                className: 'control',
+                orderable: false,
+                targets: -1
             }
         ],
-        order: [[0, 'desc']],
-        dom: '<"datatable-header"fl><"datatable-scroll-lg"t><"datatable-footer"ip>',
+        order: [[ 0, 'asc' ]],
+        dom: '<"datatable-header datatable-header-accent"fBl><""t><"datatable-footer"ip>',
         language: {
-            search: '<span>Filter:</span> _INPUT_',
+            search: '<span>Search people:</span> _INPUT_',
             searchPlaceholder: 'Type to filter...',
             lengthMenu: '<span>Show:</span> _MENU_',
-            paginate: {'first': 'First', 'last': 'Last', 'next': '&rarr;', 'previous': '&larr;'}
+            paginate: { 'first': 'First', 'last': 'Last', 'next': '&rarr;', 'previous': '&larr;' }
         },
-        lengthMenu: [15, 25, 50, 75, 100],
-        displayLength: 25,
-        drawCallback: function (settings) {
-            var api = this.api();
-            var rows = api.rows({page: 'current'}).nodes();
-            var last = null;
-
-            // Grouod rows
-            api.column(1, {page: 'current'}).data().each(function (group, i) {
-                if (last !== group) {
-                    $(rows).eq(i).before(
-                        '<tr class="active border-double"><td colspan="8" class="text-semibold">' + group + '</td></tr>'
-                    );
-
-                    last = group;
+        lengthMenu: [ 25, 50, 75, 100 ],
+        displayLength: 50,
+        responsive: {
+            details: {
+                type: 'column',
+                target: -1
+            }
+        },
+        buttons: [
+            {
+                extend: 'pdfHtml5',
+                text: 'Export list <i class="icon-file-pdf position-right"></i>',
+                className: 'btn bg-blue',
+                orientation: 'landscape',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 3, 4, 5 ],
+                    stripHtml: true
+                },
+                customize: function (doc) {
+                    doc.content[1].table.widths = Array(doc.content[1].table.body[0].length + 1).join('*').split('');
                 }
-            });
-
-            // Datepicker
-            $(".datepicker").datepicker({
-                showOtherMonths: true,
-                dateFormat: "d MM, y"
-            });
-
-            // Select2
-            $('.select').select2({
-                width: '150px',
-                minimumResultsForSearch: Infinity
-            });
-
-            // Reverse last 3 dropdowns orientation
+            }
+        ],
+        drawCallback: function (settings) {
             $(this).find('tbody tr').slice(-3).find('.dropdown, .btn-group').addClass('dropup');
         },
-        preDrawCallback: function (settings) {
-
-            // Reverse last 3 dropdowns orientation
+        preDrawCallback: function(settings) {
             $(this).find('tbody tr').slice(-3).find('.dropdown, .btn-group').removeClass('dropup');
-
-            // Destroy Select2
-            $('.select').select2().select2('destroy');
         }
     });
 
--- profile/models.py
+++ profile/models.py
@@ -4,13 +4,12 @@ from anthill.framework.db import db
 from anthill.framework.conf import settings
 from anthill.framework.utils.asynchronous import as_future
 from anthill.platform.api.internal import InternalAPIMixin
-from sqlalchemy_utils.types.json import JSONType
+from sqlalchemy_utils.types import JSONType
 from jsonpath_ng.ext import parser
-from types import FunctionType
 
 
 class Profile(InternalAPIMixin, db.Model):
-    """Extra fields for User model."""
+    """Extra data for User model."""
     __tablename__ = 'profiles'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -20,19 +19,19 @@ class Profile(InternalAPIMixin, db.Model):
     async def get_user(self):
         return await self.internal_request('login', 'get_user', user_id=self.user_id)
 
-    # noinspection PyMethodMayBeStatic
-    def _parse_obj(self, path: str):
+    @staticmethod
+    def _payload_parse_obj(path: str):
         return parser.parse(path, debug=settings.DEBUG)
 
     @as_future
     def find_payload(self, path: str):
-        return self._parse_obj(path).find(self.payload)
+        return self._payload_parse_obj(path).find(self.payload)
 
     @as_future
-    def filter_payload(self, path: str, fn: FunctionType):
-        return self._parse_obj(path).filter(fn, self.payload)
+    def filter_payload(self, path: str, fn):
+        return self._payload_parse_obj(path).filter(fn, self.payload)
 
     @as_future
     def update_payload(self, path: str, data, commit=True):
-        self.payload = self._parse_obj(path).update(self.payload, data)
+        self.payload = self._payload_parse_obj(path).update(self.payload, data)
         self.save(commit)
