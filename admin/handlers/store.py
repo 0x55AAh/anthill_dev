109c3e996287403d29c31d007a48969a7bb7a90b
Vladimir <x55aah@gmail.com>
2019-04-03 20:18:23 +0300

    update admin

--- admin/handlers/store.py
+++ admin/handlers/store.py
@@ -12,3 +12,98 @@ class IndexHandler(StorePageHandler):
         context = await super().get_context_data(**kwargs)
         context['items_list'] = []  # TODO:
         return context
+
+
+class TierListHandler(StorePageHandler):
+    page_name = 'tier_list'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        context['tiers'] = []  # TODO:
+        return context
+
+
+class TierDetailHandler(StorePageHandler):
+    page_name = 'tier_detail'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        tier_id = self.path_kwargs['tier_id']
+        context['tier'] = {}  # TODO:
+        return context
+
+
+class StoreListHandler(StorePageHandler):
+    page_name = 'store_list'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        context['stores'] = []  # TODO:
+        return context
+
+
+class StoreDetailHandler(StorePageHandler):
+    page_name = 'store_detail'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        store_id = self.path_kwargs['store_id']
+        context['store'] = {}  # TODO:
+        return context
+
+
+class OrderListHandler(StorePageHandler):
+    page_name = 'order_list'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        context['orders'] = []  # TODO:
+        return context
+
+
+class OrderDetailHandler(StorePageHandler):
+    page_name = 'order_detail'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        order_id = self.path_kwargs['order_id']
+        context['order'] = {}  # TODO:
+        return context
+
+
+class ItemCategoryListHandler(StorePageHandler):
+    page_name = 'item_category_list'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        context['item_categories'] = []  # TODO:
+        return context
+
+
+class ItemCategoryDetailHandler(StorePageHandler):
+    page_name = 'item_category_detail'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        item_category_id = self.path_kwargs['item_category_id']
+        context['item_category'] = {}  # TODO:
+        return context
+
+
+class CurrencyListHandler(StorePageHandler):
+    page_name = 'currency_list'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        context['currencies'] = []  # TODO:
+        return context
+
+
+class CurrencyDetailHandler(StorePageHandler):
+    page_name = 'currency_detail'
+
+    async def get_context_data(self, **kwargs):
+        context = await super().get_context_data(**kwargs)
+        currency_id = self.path_kwargs['currency_id']
+        context['currency'] = {}  # TODO:
+        return context
--- admin/routes/event.py
+++ admin/routes/event.py
@@ -9,16 +9,21 @@ from admin.handlers import event as handlers, LogRequestHandler
 def route_patterns():
     return [
         url(r'^/?$', handlers.IndexHandler, name='index'),
+
         url(r'^/(?P<event_id>[^/]+)/?$', handlers.EventDetailHandler, name='event_detail'),
-        url(r'^/generator_pools/?$', handlers.GeneratorPoolListHandler, name='generator_pool_list'),
-        url(r'^/generator_pools/(?P<generator_pool_id>[^/]+)/?$', handlers.GeneratorPoolDetailHandler,
-            name='generator_pool_detail'),
-        url(r'^/generators/?$', handlers.GeneratorListHandler, name='generator_list'),
-        url(r'^/generators/(?P<generator_id>[^/]+)/?$', handlers.GeneratorDetailHandler, name='generator_detail'),
-        url(r'^/categories/?$', handlers.CategoryListHandler, name='category_list'),
-        url(r'^/categories/(?P<category_id>[^/]+)/?$', handlers.CategoryDetailHandler, name='category_detail'),
         url(r'^/(?P<event_id>[^/]+)/participants/?$', handlers.ParticipantListHandler, name='participant_list'),
         url(r'^/(?P<event_id>[^/]+)/participants/(?P<user_id>[^/]+)/?$', handlers.ParticipantDetailHandler,
             name='participant_detail'),
+
+        url(r'^/generator_pools/?$', handlers.GeneratorPoolListHandler, name='generator_pool_list'),
+        url(r'^/generator_pools/(?P<generator_pool_id>[^/]+)/?$', handlers.GeneratorPoolDetailHandler,
+            name='generator_pool_detail'),
+
+        url(r'^/generators/?$', handlers.GeneratorListHandler, name='generator_list'),
+        url(r'^/generators/(?P<generator_id>[^/]+)/?$', handlers.GeneratorDetailHandler, name='generator_detail'),
+
+        url(r'^/categories/?$', handlers.CategoryListHandler, name='category_list'),
+        url(r'^/categories/(?P<category_id>[^/]+)/?$', handlers.CategoryDetailHandler, name='category_detail'),
+
         url(r'^/log/?$', LogRequestHandler, {'service_name': 'event'}, name='log')
     ]
